ec38ca482501c86796a9b06b4aca9b011c730c50
bernatskyi.mykola <bernatskyi.mykola@gmail.com>
2022-01-02 22:01:14 +0200

    Add  HW_03 : task 1_2 - Done!!!

--- Python/HW_01 (Basic)/HW_1_2.py
+++ Python/HW_01 (Basic)/HW_1_2.py
@@ -1,9 +1,10 @@
 # FizzBuzz - надрукуйте числа від 1 до 60. Проте якщо число ділиться на 3 друкуйте fizz, якщо ділиться на 5 - buzz, якщо і на 3 і на 5 – fizzbuzz
 for x in range(60):
-    if not (x+1)%3 and not (x+1)%5:
+    if not (x+1) % 3 and not (x+1) % 5:
         print("fizzbuzz")
-    elif not (x+1)%3:
+    elif not (x+1) % 3:
         print("fizz")
     elif not (x + 1) % 5:
         print("buzz")
-    else: print(x + 1)
+    else:
+        print(x + 1)
